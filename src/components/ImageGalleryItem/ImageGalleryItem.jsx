366d4671c350805d28ebcae5cb7c24f001cb10fc
Denys iElkin <hvoarang@gmail.com>
2023-02-09 17:11:20 +0200

    fix

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -16,42 +16,6 @@ export class ImageGalleryItem extends Component {
 		page: 1,
 	};
 
-// componentDidUpdate(prevProps, prevState) {
-
-//     const prevName = prevProps.pictureName;
-//     const nextName = this.props.pictureName;
-
-//     if (prevName !== nextName || prevProps.page !== this.props.page) {
-//       this.setState({ status: 'pending' });
-//       fetch(
-//         `https://pixabay.com/api/?key=30188336-4b4f26bd7e50c24034d7e6b40&q=${nextName}&page=${this.props.page}&per_page=20`
-//       )
-//         .then(r => r.json())
-//         .then(images => {
-//           if (!images.hits.length) {
-//             toast('unfortunately there are no images with such query');
-//             this.setState({ status: 'idle' });
-//             this.setState({ picture: [] });
-//             return;
-//           }
-//           this.setState({
-//             picture:
-//                  [...this.prevstate.picture, ...images.hits],
-//             status: 'resolved',
-//           });
-          
-
-          
-//         })
-//         .finally(() =>
-//           setTimeout(() => {
-//             if (this.props.page > 1) {
-//               this.onScroll();
-//             }
-//           }, 400)
-//         );
-//     }
-//   }
 
  
 	componentDidUpdate(prevProps, prevState) {
